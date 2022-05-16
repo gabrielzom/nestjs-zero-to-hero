a100a31d403dfe778c241fd97cb954d74a1c2689
Gabriel Nascimento <gabrielnascimento_17@hotmail.com>
2022-05-16 13:18:12 -0300

    feature: in this commit is showing the methods creation of controllers and services, besides create interface and enum to Task

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { Task } from './tasks.model';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getAllTasks(): Task[] {
+    return this.tasksService.getAllTasks();
+  }
+
+  @Post()
+  createTask(@Body() task: Task): Task {
+    console.log('Created task ---> ', task);
+    const { title, description } = task;
+    return this.tasksService.createTask(title, description);
+  }
 }
