4892537669dbf79f001a64997545e7b5a67e84d3
Gabriel Nascimento <gabrielnascimento_17@hotmail.com>
2022-08-23 23:34:00 -0300

    [refactor/NZTH/PERS-1] feat: add with nest scafolding resource of projects

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { UsersModule } from './users/users.module';
+import { ProjectsModule } from './projects/projects.module';
+import { TestsModule } from './tests/tests.module';
+import { ProjectsModule } from './projects/projects.module';
 
 @Module({
-  imports: [TasksModule, UsersModule],
+  imports: [TasksModule, UsersModule, ProjectsModule, TestsModule],
 })
 export class AppModule {}
