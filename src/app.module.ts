48cf758f305e845f85be9d2afe3cf13b2b1e65b3
Gabriel Nascimento <gabrielnascimento_17@hotmail.com>
2022-08-24 01:38:32 -0300

    [refactor/NZTH/PERS-1] refact: added endpoint for register user

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,8 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { UsersModule } from './users/users.module';
 import { ProjectsModule } from './projects/projects.module';
-import { TestsModule } from './tests/tests.module';
-import { ProjectsModule } from './projects/projects.module';
 
 @Module({
-  imports: [TasksModule, UsersModule, ProjectsModule, TestsModule],
+  imports: [TasksModule, UsersModule, ProjectsModule],
 })
 export class AppModule {}
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,18 +1,29 @@
 import { ApiProperty } from '@nestjs/swagger/dist/decorators';
+import { IsEnum } from 'class-validator';
+import { UserRoleEnum } from '../utils/users.options';
 
 export class CreateUserDto {
-  @ApiProperty({ type: 'string', format: 'text' })
+  @ApiProperty({ type: 'string', format: 'text', example: 'albert' })
   name: string;
 
-  @ApiProperty({ type: 'string', format: 'text' })
+  @ApiProperty({ type: 'string', format: 'text', example: 'einstein' })
   lastName: string;
 
-  @ApiProperty({ type: 'string', format: 'text' })
+  @ApiProperty({
+    type: 'string',
+    format: 'email',
+    example: 'albert.einstein@example.com',
+  })
   email: string;
 
-  @ApiProperty({ type: 'string', format: 'text' })
+  @ApiProperty({ type: 'string', format: 'text', example: 'your_password' })
   password: string;
 
-  @ApiProperty({ type: 'string', format: 'text' })
-  role: string;
+  @ApiProperty({
+    type: 'string',
+    format: 'text',
+    enum: UserRoleEnum,
+  })
+  @IsEnum(UserRoleEnum)
+  role: UserRoleEnum;
 }
